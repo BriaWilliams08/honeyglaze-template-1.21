b6b43fe9e15e5ca5c4ed148cce9e086353f09f2e
BriaWilliams08 <briawilliams827@gmail.com>
2025-07-10 21:51:49 -0400

    Initial commit

---
+++ src/main/java/com/honeyglaze/HoneyGlazeClient.java
@@ -0,0 +1,10 @@
+package com.honeyglaze;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HoneyGlazeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {}
+    
+}
